0be108ec86954408fdbe5196148f0594e3647879
Shohruh Abdullayev <abdullayevshohruh343@gmail.com>
2024-09-28 20:57:06 +0500

    1.uy ishi

---
+++ 5-oy/1-uy ishi/4.py
@@ -0,0 +1,40 @@
+from PyQt5.QtWidgets import (QWidget, QApplication, QLineEdit, QPushButton, QLabel)
+from PyQt5.QtGui import QFont
+from googletrans import Translator 
+
+app = QApplication([])
+
+win = QWidget()
+win.setWindowTitle("Matn Tarjimon")
+win.setGeometry(600, 300, 400, 250)  
+
+font = QFont("Arial", 14)
+
+line_edit = QLineEdit(win)
+line_edit.setGeometry(50, 30, 300, 40)  
+line_edit.setFont(font)
+
+label = QLabel(win)
+label.setGeometry(50, 80, 300, 40)  
+label.setFont(font)
+label.setText("Tarjima natijasi")
+
+# Button
+button = QPushButton("Tarjima qilish", win)
+button.setGeometry(150, 140, 150, 50)  
+button.setFont(font)
+
+
+translator = Translator()
+
+
+def translate_text():
+    input_text = line_edit.text()  
+    translated = translator.translate(input_text, src='uz', dest='en')  
+    label.setText(translated.text)  
+
+
+button.clicked.connect(translate_text)
+
+win.show()
+app.exec_()
---
+++ 5-oy/1-uy ishi/5.py
@@ -0,0 +1,87 @@
+from PyQt5.QtWidgets import (QWidget, QApplication, QVBoxLayout, QGridLayout, QPushButton, QLabel)
+from PyQt5.QtGui import QFont
+
+class Calculator(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("kalc.py")
+        self.setGeometry(600, 300, 300, 400)  
+
+        self.display = QLabel('0', self)
+        self.display.setFont(QFont('Arial', 24))
+        self.display.setStyleSheet("background:white;")
+        self.display.setFixedHeight(60)
+        
+        
+        vbox = QVBoxLayout()
+        grid = QGridLayout()
+
+        
+        buttons = {
+            '1': (1, 0), '2': (1, 1), '3': (1, 2), '+': (1, 3),
+            '4': (2, 0), '5': (2, 1), '6': (2, 2), '-': (2, 3),
+            '7': (3, 0), '8': (3, 1), '9': (3, 2), '=': (3, 3),
+            'C': (4, 0), '0': (4, 1)
+        }
+
+        
+        for btn_text, pos in buttons.items():
+            button = QPushButton(btn_text)
+            button.setFont(QFont('Arial', 18))
+            button.setFixedSize(60, 60)
+            grid.addWidget(button, *pos)
+            button.clicked.connect(self.on_click)
+
+        vbox.addWidget(self.display)
+        vbox.addLayout(grid)
+
+        self.setLayout(vbox)
+ 
+        self.current_input = ''
+        self.last_input = ''
+        self.operator = ''
+    
+    def on_click(self):
+        button = self.sender()
+        text = button.text()
+
+        if text.isdigit() or text == '0':
+            if self.display.text() == '0' or self.operator:
+                self.display.setText(text)
+            else:
+                self.display.setText(self.display.text() + text)
+
+            self.current_input += text
+
+        elif text in ['+', '-']:
+            if self.current_input:
+                self.last_input = self.current_input
+                self.operator = text
+                self.current_input = ''
+
+        elif text == '=':
+            if self.last_input and self.current_input:
+                if self.operator == '+':
+                    result = int(self.last_input) + int(self.current_input)
+                elif self.operator == '-':
+                    result = int(self.last_input) - int(self.current_input)
+
+                self.display.setText(str(result))
+                self.current_input = str(result)
+                self.last_input = ''
+                self.operator = ''
+
+        elif text == 'C':
+            self.display.setText('0')
+            self.current_input = ''
+            self.last_input = ''
+            self.operator = ''
+
+app = QApplication([])
+win = Calculator()
+win.show()
+app.exec_()
